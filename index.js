4d2681e222664573f27af28d6fe2890dbe73dc02
Eric Brian Njuguna <eric.njuguna@student.moringaschool.com>
2024-07-10 05:55:17 +0300

    Adds functions that filter array based on specific conditions in index.js

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+const drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+
+function findMatching(array, string) {
+    return array.filter(array => array.toLowerCase() == string.toLowerCase());
+
+}
+
+// console.log(findMatching(drivers, 'bobb'));
+
+function fuzzyMatch(array, string) {
+    // .slice() helps us check the beginning characters of the strings in the array
+
+    return array.filter(array => array.slice(0, 2) == string);
+
+    // return array.filter(array => array.name.slice(0, 2) == string);
+}
+
+// console.log(fuzzyMatch(drivers, 'Sa'));
+
+function matchName(array, string) {
+    return array.filter(array => array.name == string);
+}
+
+console.log(matchName(drivers, `Bobby`));
